a0d3ab483792cf6f68581dff6989b78045df0893
Harry-Reyes <114123042+harry-reyes@users.noreply.github.com>
2022-10-16 12:37:18 +0800

    updated linear search

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Data Structures and Algorithms
+
+## Harry Reyes | BSIS 2
+
+---
+
+## Linear Search Algorithm
+
+### Functions
+
+    getEntries() - User is requested to input a string list in a format of numbers separated by commas and the value converts into an integer list. Then, a number to find is requested.
+
+    searchFrom_left() - It is the first implementation of the linear search algorithm script which compares numbers by index from left to right.
+
+    searchFrom_right() - This is the second implementation of the script. The comparison is the same, but the list is reversed to say that it started from the lastest index.
+
+    use_isFound() - The variable isFound(), from searchFrom_left(), is used to confirm if any index matches with a number. It prints the index of a matching number and the steps made.
+
+### Flow
+
+    Starting Point: getEntries()
+    Next: searchFrom_left()
+    Ending Point: use_isFound()
+        # if isFound() = true: searchFrom_right()
--- search/linear_search.py
+++ search/linear_search.py
@@ -1,25 +1,43 @@
-def getEntry():
-    print("Type in your entry.")
-    entry = input(str())
-def convert_entry_to_int_array():
+def getEntries():
     global entry
-    global looking_for
-    global memory
-    entry = ""
-    comma = ','
-    steps = 0
-    index = 0
+    global entry_numberToSearch
+    entry = ''
+    entry_numberToSearch = 0
+    entry = list(map(int, input("Type your entry: ").split(',')))
+    entry_numberToSearch = int(input("Number to search: "))
+    print("Your entry is", entry)
+def searchFrom_left():
+    global isFound
+    global stepsFrom_left
+    global entry_index
+    entry_index = 0
+    isFound = False
+    stepsFrom_left = 0
+    while entry_index <= len(entry)-1:
+        stepsFrom_left += 1
+        if entry_numberToSearch == entry[entry_index]:
+            isFound = True
+            break
+        else:
+            entry_index += 1
+def searchFrom_right():
+    global stepsFrom_right
+    stepsFrom_right = 0
     i = 0
-    isDone = False
-    entry = chr(entry)
-    for isDone in range(False, True):
-        for comma in range(','):
-            if entry[index] == ',':
-                index += 1
-            elif entry[index] == '':
-                isDone = True
-                return
-        memory[index] = entry[index]
-        print("Memory value '", memory[index], "' retrieved")
-getEntry()
-convert_entry_to_int_array()
+    entry_reversed = list(reversed(entry))
+    while i <= len(entry_reversed)-1:
+        stepsFrom_right += 1
+        if entry_numberToSearch == entry_reversed[i]:
+            break
+        else:
+            i += 1
+def use_isFound():
+    if isFound == False:
+        print("Not found")
+        exit()
+    else:
+        searchFrom_right()
+        print("Index:", entry_index, "\nSteps taken:\nFrom left =", stepsFrom_left, "\nFrom right =", stepsFrom_right)
+getEntries()
+searchFrom_left()
+use_isFound()
